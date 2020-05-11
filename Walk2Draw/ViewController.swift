df07fde87fecf42e6c3fd4267183f99893b687f5
montyboyer <montyboyer@gmail.com>
2020-05-11 16:56:39 -0700

    Initial Commit

---
+++ Walk2Draw/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Walk2Draw
+//
+//  Created by Monty Boyer on 5/11/20.
+//  Copyright © 2020 Monty Boyer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
